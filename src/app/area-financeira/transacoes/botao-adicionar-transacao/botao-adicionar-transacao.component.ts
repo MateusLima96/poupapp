ed10d7d480b553a2a387ecfa57bac2cf874702bb
Mateus de Araújo Lima <mateuslima020@outlook.com>
2026-03-24 17:57:34 -0300

    feat: creates transaction obj

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,17 +1,39 @@
 import { Component, effect, signal } from '@angular/core';
 import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
-
+import { FormsModule } from "@angular/forms"
+import { KeyValuePipe } from '@angular/common';
+import { TipoTransacao, Transacao } from '../../compartilhados/transacao.model';
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent, ModalComponent],
+  imports: [BotaoComponent, ModalComponent, FormsModule, KeyValuePipe],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css'
 })
 export class BotaoAdicionarTransacaoComponent {
     modalAberto = signal(false);
 
+    novaTransacaoForm = {
+      nome: '',
+      tipo: '',
+      valor: 0,
+      data: '',
+      conta: ''
+    }
+
+    tiposTransacao = TipoTransacao
+
     abrirModal() {
       this.modalAberto.set(true);
+    }
+
+    aoSubmeter() {
+      const novaTransacao = new Transacao(
+        this.novaTransacaoForm.nome,
+        this.novaTransacaoForm.tipo as TipoTransacao,
+        this.novaTransacaoForm.valor,
+        this.novaTransacaoForm.data,
+        this.novaTransacaoForm.conta
+      );
     }
 }
